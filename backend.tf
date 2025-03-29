5b2ed93b8130aee8d97da6517285311ab6f92a75
Carlos Adriano <souza.adriano.carlos@gmail.com>
2025-03-29 04:09:45 -0300

    commit 0409

---
+++ backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  # cloud {
+  #   organization = "adriano-org"
+
+  #   workspaces {
+  #     name = "devops-aws-myapp-dev"
+  #   }
+  # }
+}
+
+
+
+# provider "aws" {
+#   region = "us-east-1"
+  
+# }
+
+provider "aws" {
+  region     = "us-east-1"
+  
+}
---
+++ first_ec2.tf
@@ -0,0 +1,19 @@
+##https://github.com/zealvora/terraform-beginner-to-advanced-resource/blob/master/Section%201%20-%20Deploying%20Infrastructure%20with%20Terraform/first-ec2.md
+
+
+###Intancia Linux
+resource "aws_instance" "myec2-0736" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "myec2-${formatdate("mmss", timestamp())}"
+    created = formatdate("YYYYMMDD-HHmmss", timestamp())
+    Create_date_time = formatdate("YYYY-MM-DD hh:mm:ss ZZZ", timestamp())
+    created2 = formatdate("YYYYMMDDhhmmss", timestamp())
+    created-date = timestamp()
+    env = hml
+  }
+    #lifecycle { ignore_changes = [tags["Create_date_time"], tags["Name"]]}
+    #lifecycle { ignore_changes = [tags["Create_date_time"]]}
+}
+
